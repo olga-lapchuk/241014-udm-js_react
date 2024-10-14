be7aa0921ab3daea8fc2f55c0a56b2d728dfa9fd
Olga <lapchuk.olga@gmail.com>
2024-10-14 20:18:19 +0300

    upd script

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-console.log('arr'+"-object");
 
-console.log(4 + + "5");
-
-let incr = 10;
-    decr = 10;
-
-// incr++;
-// decr--;
-
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-
-console.log(2+2*2 === 8);
 
 
 
