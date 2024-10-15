78d635324e0aeb02e07ed66acdba941d9adb008b
Olga <lapchuk.olga@gmail.com>
2024-10-15 19:09:25 +0300

    add codeа

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {};
+   actors: {};
+   genres: [];
+   privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      с = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
-
-
-const isChecked = true,
-      isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(personalMovieDB);
