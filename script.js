3ccda747e41b2e2742adeb3273484f8a956edb81
Olga <lapchuk.olga@gmail.com>
2024-10-17 10:44:43 +0300

    new for

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ const personalMovieDB = {
    privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let = 0, i < 2; i++) {
+   const a = prompt('Один из последних просмотренных фильмов?', ''),
+         b = prompt('На сколько оцените его?', '');
+   
+   personalMovieDB.movies[a] = b;
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
 
 console.log(personalMovieDB);
+
+
+
+
+
